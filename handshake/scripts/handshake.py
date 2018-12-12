9ee67b825b5875a0a1d4f16677bcfd1f2b269a4a
Jon McMillan <mcmillan.jon@gmail.com>
2018-12-12 11:38:41 -0700

    some refactoring + convert data files to have a delay

--- handshake/scripts/handshake.py
+++ handshake/scripts/handshake.py
@@ -3,49 +3,69 @@ import rospy
 import intera_interface
 import json
 import sys
+import time
 
 
 def file_path(name):
+    """ get the file path to data files.
+
+    Note: temporarily hard coded for jon's computer
+    """
     return "/home/jon/Project_Robotics/data/{}".format(name.lower())
 
 
-def record(limb):
-    FILE_NAME = "TODO"
-    with open(FILE_NAME, 'w') as fh:
-        inp = None
-        positions = []
-        while inp != "q":
-            print("Press enter to record position...")
-            inp = raw_input()
-            angles = limb.joint_angles()
-            positions.append(angles)
+def record(limb, name):
+    """ record a new handshake """
+    path = file_path("%s.json" % name)
 
-        fh.write(json.dumps(positions))
+    with open(path, 'w') as fh:
+        movements = []
+        while True:
+            print("Press enter to record position...")
+            if raw_input() == 'q':
+                break
+
+            movement = {
+                "angles": limb.joint_angles(),
+                "delay": 0.0
+            }
+            movements.append(movement)
+
+        fh.write(json.dumps(movements))
+
+    rospy.loginfo('recording complete. file saved at {}'.format(path))
 
 
 def playback(hd, limb, name):
-    limb.move_to_neutral()
+    """ playback a handshake """
+    rospy.loginfo('playing %s' % name)
+    limb.move_to_neutral(timeout=1)
     hd.display_image(file_path("{}.jpg".format(name)))
 
     with open(file_path("{}.json".format(name))) as fh:
-        positions = json.loads(fh.read())
+        movements = json.loads(fh.read())
 
-        for i, position in enumerate(positions):
-            print('Moving to position: {}'.format(i+1))
-            limb.move_to_joint_positions(position)
+        for i, movement in enumerate(movements):
+            rospy.loginfo('Moving to position: {}'.format(i+1))
+            limb.move_to_joint_positions(movement['angles'], timeout=0.5)
 
-    print('exit')
+            rospy.loginfo('pausing {:.1f} seconds'.format(movement['delay']))
+            time.sleep(movement['delay'])
+
+    limb.move_to_neutral(timeout=1)
+    rospy.loginfo('playback done')
 
 
 def init_sawyer():
+    """ basic setup stuff """
     rospy.loginfo("initializing sawyer...")
 
     limb = intera_interface.Limb('right')
     hd = intera_interface.HeadDisplay()
     head = intera_interface.Head()
 
-    limb.move_to_neutral()
-    head.set_pan(0.0)
+    limb.move_to_neutral(timeout=1)
+    head.set_pan(0.0, timeout=1)
 
     rospy.loginfo("sawyer initialized")
 
@@ -54,11 +74,15 @@ def init_sawyer():
 
 if __name__ == '__main__':
     rospy.init_node('handshake')
-    print('Node initialized')
+    rospy.loginfo('handshake node initialized')
 
     limb, hd, _ = init_sawyer()
 
-    if len(sys.argv) > 1 and sys.argv[1] == 'record':
-        record(limb)
+    if len(sys.argv) <= 1:
+        print('usage: rosrun handshake handshake.py <name>')
+        exit(1)
+
+    if sys.argv[1] == 'record':
+        record(limb, sys.argv[2])
     else:
-        playback(hd, limb, sys.argv[2])
+        playback(hd, limb, sys.argv[1])
