a78ada33d3ec06aabc14477e5b250a994340c9c7
Jon McMillan <mcmillan.jon@gmail.com>
2018-12-16 18:09:35 -0700

    publish from webserver

--- handshake/scripts/webserver.py
+++ handshake/scripts/webserver.py
@@ -3,23 +3,23 @@
 import SimpleHTTPServer
 import SocketServer
 import time
-import handshake
 import rospy
 import os
+from std_msgs.msg import String
 
 
-HOST_NAME = os.getenv('HOST')
-PORT_NUMBER = 8080
+HOST_NAME = os.getenv('HOST', 'localhost')
+PORT_NUMBER = os.getenv('PORT', 8080)
 
 
 def playback(name):
-    limb, hd, _ = handshake.init_sawyer()
-    print(limb)
-    handshake.playback(hd, limb, name)
+    name = name.lower()
+    rospy.loginfo('publishing %s' % name)
+    rospy.Publisher('handshake/play', String, queue_size=1, latch=True).publish(String(name))
 
 
 def edit(name):
-    print('not implemented yet {}'.format(name))
+    rospy.loginfo('edit not implemented yet {}'.format(name))
 
 
 ACTIONS = {
@@ -36,10 +36,11 @@ class Server(SimpleHTTPServer.SimpleHTTPRequestHandler):
         return
 
     def do_GET(self):
+        rospy.loginfo('Request: %s' % self.path)
         _, action, name = str(self.path).split('/')
         func = ACTIONS[action]
         func(name)
-        return
+        self.send_response(204)
 
     def handle_http(self):
         return
@@ -51,10 +52,11 @@ class Server(SimpleHTTPServer.SimpleHTTPRequestHandler):
 if __name__ == '__main__':
     rospy.init_node('webserver')
     httpd = SocketServer.TCPServer((HOST_NAME, PORT_NUMBER), Server)
-    print(time.asctime(), 'Server UP - %s:%s' % (HOST_NAME, PORT_NUMBER))
+    rospy.loginfo("%s Server Up - %s %s" % (time.asctime(), HOST_NAME, PORT_NUMBER))
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
-    # httpd.server_close()
+
+    httpd.server_close()
     print(time.asctime(), 'Server DOWN - %s:%s' % (HOST_NAME, PORT_NUMBER))
--- website/main.js
+++ website/main.js
@@ -2,13 +2,13 @@
 // Setup all event listeners here.
 function onPageLoad() {
     //save
-    let btns = document.querySelectorAll('.js-playback');
-    btns.forEach(btn => btn.addEventListener('click', playback));
+    document.querySelectorAll('.js-playback')
+        .forEach(btn => btn.addEventListener('click', playback));
 }
 
 function playback(event) {
     const name = event.target.textContent;
-    fetch(`http://localhost:8080/playback/${name}`)
+    fetch(`http://localhost:8080/playback/${name}`, {mode: 'no-cors'})
         .then(() => alert('done'));
 }
 
